bf10df2397cf57db2f738c8f9fa1bbaad1cf0f80
정경환 <sujkh@nomadconnection.com>
2018-04-04 14:30:02 +0900

    create icon step comopnent

--- src/etc/iconstep/IconStep.jsx
+++ src/etc/iconstep/IconStep.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
-class IconStep extends Component {
+class StepList extends Component {
+  getStepClassName=(index)=>{
+    const {step=0} = this.props;
+    let result = ''
+    if(index === step){
+      result = 'on'
+    } else if(index < step){
+      result = 'done';
+    }
+    
+    return `icon-step-list ${result}`
+  }
+
   render() {
+    const {list=['1', '2'], description, step=0} = this.props;
     return (
-      <div>
-        IconStep
+      <div className="icon-step"> {/* step (진행 단계도) */}
+        <div className="icon-step-container">
+          {list.map((item, index)=>{
+            return(
+              <div className={this.getStepClassName(index)} key={index}> {/* 완료된 step-list에게는 done 클래스 */}
+                <div className="icon-step-title">{item}</div>
+                <div className="icon-step-circle"></div>
+              </div>
+            )
+          })}
+        </div>
+        {description && <div className="icon-step-description">  {/* 현재 진행 중인 단계의 설명 */}
+          {description[step]}
+        </div>}
       </div>
     );
   }
 }
 
-export default IconStep;
+export default StepList;
--- src/etc/iconstep/IconStepContainer.jsx
+++ src/etc/iconstep/IconStepContainer.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import IconStep from './IconStep';
 
 class IconStepContainer extends Component {
   render() {
     return (
       <div>
-        IconStepContainer
+        <IconStep list={['step1', 'step2']} description={['description1','description2']} step={1}/>
       </div>
     );
   }
