6444ac83a7bcb80334e77c371e56a9b07391b514
정경환 <sujkh@nomadconnection.com>
2018-04-05 12:15:14 +0900

    code component work

--- src/common/Code.jsx
+++ src/common/Code.jsx
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
+// import Clipboard from 'react-clipboard.js';
 
 class Code extends Component {
+  getImportRenderer=()=>{
+    let importRender= [];
+    const {importFrom} = this.props
+    importFrom.forEach((item, index)=>{
+      let key = Object.keys(item)[0]
+      let value = item[key]
+      importRender.push(
+        <div key={key}>
+          <span className="bold">import</span> {key} <span className="bold">from</span> <span className="red">'{value}'</span>;  
+        </div>
+      )
+    })
+    return importRender;
+  }
+
+  getLibraryObjectRenderer=()=>{
+    const {libraryObject} = this.props
+    console.log('libraryObject', libraryObject)
+    return(
+      <div>
+        <span className="blue">{'<'}{libraryObject.name}</span> <br/>
+          {libraryObject.param.map((item)=>{
+            let key = Object.keys(item)[0]
+            let value = item[key]
+            return(
+              <div>
+                &nbsp;&nbsp;<span className="blue">{key}=</span>
+                <span className="red">{value}</span>
+              </div>
+            )
+          })}
+        <span className="blue">{'/>'}</span> 
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className="icon-code-container">
-        {this.props.children}
+        <code>
+          {this.getImportRenderer()}<br/>
+          {this.getLibraryObjectRenderer()}
+          {this.props.children}
+        </code>
       </div>
     );
   }
--- src/etc/iconstep/IconStepContainer.jsx
+++ src/etc/iconstep/IconStepContainer.jsx
@@ -4,13 +4,25 @@ import MainTemplete from '../../common/layout/MainTemplete';
 import Code from '../../common/Code';
 
 class IconStepContainer extends Component {
+  importCode=[
+    {React:'react'},
+    {IcxComponent:'react-icx-component'}
+  ]
+
+  libraryObject={
+    name:'IconStep',
+    param:[
+      {stepList:"{['step1', 'step2', 'step3']}"}, 
+      {descriptionList:"{['description1','description2','description3']}"},
+      {step:"{1}"}
+    ],
+  }
+
   render() {
     return (
       <MainTemplete>
         <div>
-          <Code>
-            {"<IconStep stepList={['step1', 'step2', 'step3']} descriptionList={['description1','description2','description3']} step={1}/>"}
-          </Code>
+          <Code importFrom={this.importCode} libraryObject={this.libraryObject}/>
           <IconStep stepList={['step1', 'step2', 'step3']} descriptionList={['description1','description2','description3']} step={1}/>
         </div>
       </MainTemplete>
