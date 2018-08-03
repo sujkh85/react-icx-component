e230fe1f06da673b6d402d2c5710de0164bf75c8
정경환 <sujkh@nomadconnection.com>
2018-08-03 12:35:49 +0900

    docs

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,6 +1,6 @@
 // production config
 const merge = require('webpack-merge');
-const {resolve} = require('path');
+const { resolve } = require('path');
 const commonConfig = require('./common');
 const paths = require('./paths');
 
@@ -9,14 +9,14 @@ process.env.BABEL_ENV = 'production';
 
 module.exports = merge(commonConfig, {
   entry: [
-    paths.appIndexJs,// the entry point of our app
+    paths.appIndexJs, // the entry point of our app
     paths.appStyleCss
   ],
   devtool: 'source-map',
   output: {
     filename: 'js/bundle.[hash].min.js',
-    path: resolve(__dirname, '../../dist'),
-    publicPath: './',
+    path: resolve(__dirname, '../../docs'),
+    publicPath: './'
   },
-  plugins: [],
+  plugins: []
 });
