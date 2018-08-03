f4371b84767832af6c0e37858b7dffbf0441a710
정경환 <sujkh@nomadconnection.com>
2018-08-03 16:51:52 +0900

    change main page

--- src/config/ConfigRouter.js
+++ src/config/ConfigRouter.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import PageNotFound from '../error/PageNotFound';
 import PageNotConnect from '../error/PageNotConnect';
-import {QuerySwitch, QueryRoute} from '../library/queryrouter/';
+import { QuerySwitch, QueryRoute } from '../library/queryrouter/';
 import IntroContainer from '../intro/IntroContainer';
 import EtcContainer from '../etc/EtcContainer';
 import ButtonContainer from '../buttons/ButtonContainer';
 import ChartContainer from '../chart/ChartContainer';
 import UtilContainer from '../util/UtilContainer';
 
-const ConfigRouter = (props) => {
-	return(
-		<QuerySwitch {...props} queryname="pagename">
-			<QueryRoute component={IntroContainer} />
+const ConfigRouter = props => {
+  return (
+    <QuerySwitch {...props} queryname="pagename">
+      <QueryRoute component={EtcContainer} />
       <QueryRoute path="intro" component={IntroContainer} />
-			<QueryRoute path="etc" component={EtcContainer} />
-			<QueryRoute path="chart" component={ChartContainer} />
-			<QueryRoute path="button" component={ButtonContainer} />
-			<QueryRoute path="util" component={UtilContainer} />
-			
-			<QueryRoute path="/notconnect" component={PageNotConnect}/>
-			<QueryRoute nomatch component={PageNotFound}/>
-		</QuerySwitch>
-	)
-}
+      <QueryRoute path="etc" component={EtcContainer} />
+      <QueryRoute path="chart" component={ChartContainer} />
+      <QueryRoute path="button" component={ButtonContainer} />
+      <QueryRoute path="util" component={UtilContainer} />
+
+      <QueryRoute path="/notconnect" component={PageNotConnect} />
+      <QueryRoute nomatch component={PageNotFound} />
+    </QuerySwitch>
+  );
+};
 
 export default ConfigRouter;
--- src/intro/components/IntroBody.jsx
+++ src/intro/components/IntroBody.jsx
@@ -10,7 +10,7 @@ class MainBody extends PureComponent {
       <div className="intro-icx-body-container">
         <section>
           <h1>HELLO ICX COMPONENT</h1>
-          <h2 onClick={this.onClickDemo}>Enter Demo</h2>
+          <h2 onClick={this.onClickDemo} style={{cursor:'pointer'}}>Enter Demo</h2>
         </section>
         <section>
           React-Icx-Component 
